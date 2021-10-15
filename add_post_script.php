fc0264d10a83d04b98652d618c5b0948aa19f141
Hugo <76749003+Hugo-hero@users.noreply.github.com>
2021-10-15 18:13:58 +0200

    Add comments and changed path name

--- add_post_script.php
+++ add_post_script.php
@@ -1,12 +1,12 @@
-<?php /* Template Name: Add post auto */ ?>
+<?php /* Template Name: Add posts */ ?>
 <?php get_header(); ?>
 <?php
-$files = glob(get_template_directory() . "/auto-posts/*.html");
-$user = get_user_by('slug', 'Hugo');
-$category = get_terms(['taxonomy' => 'category', 'name__like' => 'bourse'])[0];
+$files = glob(get_template_directory() . "/your_patht_to_your_content_here/*.html");
+$user = get_user_by('slug', 'Your_user_name_here');
+$category = get_terms(['taxonomy' => 'category', 'name__like' => 'your_category'])[0];
 foreach ( $files as $filename) {
     $content = file_get_contents($filename);
-    $title = str_replace('-', ' ', basename($filename, ".html").PHP_EOL);
+    $title = str_replace('-', ' ', basename($filename, ".html").PHP_EOL); // by default the title of the post will be taken from each file title.
     $my_post = array(
         'post_title' => $title,
         'post_content' => $content,
@@ -16,9 +16,6 @@ foreach ( $files as $filename) {
     );
     $post_id = wp_insert_post($my_post);
 
-    // add category type
-    // SHOULD BE CHANGED TO "LEXIQUE"
-    wp_set_object_terms($post_id, 'glossaire', "category_type");
     echo "$filename \n";
 }
 ?>
